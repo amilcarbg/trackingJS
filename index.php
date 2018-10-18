86cef71bb5287db3b0682051f90aec6d13ac374e
Parv Jain <parv.jain@aspiringminds.in>
2018-10-18 13:19:03 +0530

    MODIFIED

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<a href="Tag-Friends/index.html">Tag Friends</a>
-<a href="Features/index.html">Features Detection</a>
-<a href="Face/index.html">Face with Camera</a>
-<a href="Color/index.html">Color with Camera</a>
+<a href="Tag-Friends/index.html">Tag Friends</a><br>
+<a href="Features/index.html">Features Detection</a><br>
+<a href="Face/index.html">Face with Camera</a><br>
+<a href="Color/index.html">Color with Camera</a><br>
+<a href="Face-Image/index.html">Face with Image</a><br>
+<a href="Color-Image/index.html">Color with Image</a><br>
