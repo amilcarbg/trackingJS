4e21eb53d66de24f497f808300916c7f48b87f3a
Parv Jain <parv.jain@aspiringminds.in>
2018-10-18 13:01:42 +0530

    initial setup

---
+++ index.php
@@ -0,0 +1,4 @@
+<a href="Tag-Friends/index.html">Tag Friends</a>
+<a href="Features/index.html">Features Detection</a>
+<a href="Face/index.html">Face with Camera</a>
+<a href="Color/index.html">Color with Camera</a>
